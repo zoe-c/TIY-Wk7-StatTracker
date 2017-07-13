e8401d01f189f640cdf7aa3e306bf09d6b21f91a
zoe-c <find.zoec@gmail.com>
2017-07-12 20:22:57 -0400

    set up routes

--- models/activity.js
+++ models/activity.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 const activitySchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
---
+++ routes/genRoutes.js
@@ -0,0 +1,10 @@
+// GET	/activities	Show a list of all activities I am tracking, and links to their individual pages
+// POST	/activities	Create a new activity for me to track.
+// GET	/activities/{id}	Show information about one activity I am tracking, and give me the data I have recorded for that activity.
+// PUT	/activities/{id}	Update one activity I am tracking, changing attributes such as name or type. Does not allow for changing tracked data.
+// DELETE	/activities/{id}	Delete one activity I am tracking. This should remove tracked data for that activity as well.
+// POST	/activities/{id}/stats	Add tracked data for a day. The data sent with this should include the day tracked. You can also override the data for a day already recorded.
+// DELETE	/stats/{id}	Remove tracked data for a day.
+
+
+// sep if needed later
