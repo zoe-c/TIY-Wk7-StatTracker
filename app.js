4682fd42e8c131fab827122037e556322b0f04e3
zoe-c <find.zoec@gmail.com>
2017-07-13 14:40:16 -0400

    logging auth user to console, but is not working further than that. commit before trying new auth function.

--- app.js
+++ app.js
@@ -16,16 +16,16 @@ const indexRouter = require('./routes/index');
 const path = require('path');
 const bodyParser = require('body-parser');
 
-// // NOTE: uncomment when you need this for user
-// const passport = require('passport');
-// const BasicStrategy = require('passport-http').BasicStrategy;
-// const bcrypt = require('bcryptjs');
+//req auth
+const passport = require('passport');
+const BasicStrategy = require('passport-http').BasicStrategy;
+const bcrypt = require('bcryptjs');
 
 // mongoose connection
 mongoose.connect('mongodb://localhost:27017/stattracker');
-console.log('CONNECTED ON THA PORT');
 var db = mongoose.connection;
 console.log(db);
+
 
 // app set up
 const app = express();
@@ -34,7 +34,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 // *************user instances to apply auth************
 // var newUser = new User({name: "zoe", password: "zero"});
@@ -61,15 +60,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 //
 // console.log(newUser);
 
-
-
-
-
-
-
 // // use routes
-// app.use('/api', require('./routes/users'));
-app.use('/', require('./routes/index'));
+// app.use('/api/', require('./routes/routes'));
+// app.use('/', require('./routes/index'));
+
+app.get('/api/', function (req, res) {
+   res.redirect('/api/auth');
+})
+
+passport.use(new BasicStrategy(
+  function(username, password, done) {
+     console.log(username, password);
+    User.findOne({ name: username }, function(err, user){
+      console.log("FOUND A MATCH: " + user);
+      if (user && bcrypt.compareSync(password, user.password)){
+         console.log('YOU SHALL PASS: ' + user)
+        return done(null, user);
+      }
+      return done(null, false);
+    });
+  }
+));
+
+app.get('/api/auth',
+  passport.authenticate('basic', {session: false}), function (req, res) {
+      res.send('You have been authenticated, ' + req.user.username);
+  }
+);
+
+
+
 
 
 
@@ -92,8 +112,6 @@ app.use('/', require('./routes/index'));
 //       res.json(result);
 //    })
 // })
-
-
 
 
 
--- models/user.js
+++ models/user.js
@@ -17,14 +17,14 @@ const userSchema= new mongoose.Schema({
 //NOTE: uncomment after you add a few users to the collection
 // auth will be required for all routes... store this function in a var for calls in routes?
 
-// userSchema.pre('save', function (next) {
-//    if (!this.isModified('password')) {
-//       return next();
-//    }
-//    var hash = bcrypt.hashSync(this.password, 8);
-//    this.password = hash;
-//    next();
-// })
+userSchema.pre('save', function (next) {
+   if (!this.isModified('password')) {
+      return next();
+   }
+   var hash = bcrypt.hashSync(this.password, 8);
+   this.password = hash;
+   next();
+})
 
 const User = mongoose.model('User', userSchema);
 
